b4639584e47955686737d1120e90e2bc31ef1f7e
marquisalexander <marquissantos123@gmail.com>
2021-03-18 07:19:15 -0300

    learning to work with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 import { GlobalStyles } from "./styles/global";
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
 
 function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-    <>
-      <Header />
+    <TransactionsContext.Provider value={[]}>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+    <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
+
       <GlobalStyles />
-    </>
+    </TransactionsContext.Provider>
   );
 }
 
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,25 @@
-import {useEffect} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 import { Container } from './styles';
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
+
 export function TransactionsTable() {
+    const data = useContext(TransactionsContext);
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get('transactions')
-            .then(response => console.log(response.data))
+            .then(response => setTransactions(response.data.transactions))
     }, []);
 
     return (
@@ -21,18 +35,24 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">- R$1.100</td>
-                        <td>Casa</td>
-                        <td>17/02/2021</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                            <tr key={transaction.id}>
+                                <td>{transaction.title}</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL'
+                                    }).format(transaction.amount)}
+                                </td>
+                                <td>{transaction.category}</td>
+                                <td>
+                                    {new Intl.DateTimeFormat('pt-BR').format(
+                                        new Date(transaction.amount)
+                                    )}
+                                </td>
+                            </tr>
+                        )
+                    )}
                 </tbody>
             </table>
         </Container>
